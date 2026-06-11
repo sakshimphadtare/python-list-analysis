7c6b517b31d4dc13650fcdef6c631314147c0f4b
sakshimphadtare <sakshiphadtare17501@gmail.com>
2026-06-11 10:34:54 +0530

    Add statistics calculation for a list of numbers

---
+++ app.py
@@ -0,0 +1,10 @@
+numbers = [10, 25, 5, 40, 18, 60, 12, 8, 30, 50]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest number:", highest)
+print("Lowest number:", lowest)
+print("Average:", average)
